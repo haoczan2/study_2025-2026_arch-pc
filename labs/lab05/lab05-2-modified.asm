41ad7fd528a4d238b8794b613c7fcc0c00ca3fa3
haoczan2 <2821270064@qq.com>
2026-02-19 16:04:29 +0800

    Add files via upload

---
+++ labs/lab05/lab05-2-modified.asm
@@ -0,0 +1,27 @@
+%include 'in_out.asm'
+
+SECTION .data
+	msg: DB 'Введите строку: ', 0
+	echoMsg DB 'Bы Bвeли: ', 0
+
+SECTION .bss
+	buf1: RESB 80
+
+SECTION .text
+	GLOBAL _start
+_start:
+
+	mov eax, msg
+	call sprintLF
+
+	mov ecx, buf1
+	mov edx, 80
+	call sread
+
+	mov eax, echoMsg
+	call sprint
+
+	mov eax, buf1
+	call sprintLF
+
+	call quit
